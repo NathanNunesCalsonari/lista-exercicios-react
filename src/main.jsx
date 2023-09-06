8c4b5526287da7d3ca5dcf9d84365c8a44de0fb5
NathanNunesCalsonari <rm552539@fiap.com.br>
2023-09-06 20:00:00 -0300

    Configurando as rotas no main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,27 @@ import Aparelhos from './routes/EditarProdutos/index.jsx';
 import Home from './routes/Home/index.jsx';
 import Error from './routes/Error/index.jsx';
 import Error from './routes/Produtos/index.jsx';
+import EditarProdutos from './routes/EditarProdutos/index.jsx';
+import Produtos from './routes/Produtos/index.jsx';
+
+const router = createBrowserRouter([
+  { 
+    path: '/' , 
+    element: <App/>,
+    errorElement: <Error/>,
+    children: [
+      { path: '/EditarProdutos' , 
+      element: <EditarProdutos/>
+      },
+      { path: '/' , 
+      element: <Home/>
+      },
+      { path: '/Produtos' , 
+      element: <Produtos/>
+      },
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
