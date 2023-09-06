3c9027903ae590e4e1309fe0566afc5ae2a4a8d8
NathanNunesCalsonari <rm552539@fiap.com.br>
2023-09-06 20:15:21 -0300

    Passando valores pela URL em EditarProdutos

--- src/routes/EditarProdutos/index.jsx
+++ src/routes/EditarProdutos/index.jsx
@@ -1,9 +1,25 @@
-export default function EditarProdutos() {
+import { useParams, useNavigate } from "react-router-dom"
+import { listaProdutos } from "../../components/listaProdutos"
 
-    return(
-        <main>
-            <h1>Editando o produto</h1>
-        </main>
-    )
+export default function EditarProduto(){
 
+	const lista = listaProdutos
+	const navegacao = useNavigate()
+	const {id} = useParams()
+
+	const proc = lista.filter(prod => prod.id == id)
+	const produto = proc[0]
+
+	const salvar = ()=>{
+	    alert(`Produto: ${produto.nome} editado com sucesso!`)
+	    return navegacao('/produtos')
+	}
+
+	return(
+	    <main>
+		<h1>Editando o produto </h1>
+		<p>Editando os dados do produto: {produto.nome}</p>
+		<button onClick={salvar}>Salvar</button>
+	    </main>
+	)
 }
