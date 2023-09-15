ffc351703ffe784d1873e196f49403a991dbba45
NathanNunesCalsonari <rm552539@fiap.com.br>
2023-09-15 20:17:57 -0300

    Corrigindo listaProdutos

--- src/components/listaProdutos.js
+++ src/components/listaProdutos.js
@@ -1,17 +1,35 @@
 export const listaProdutos = [
-	{
-		id:1,
-		nome: 'Teclado',
-		preco: 150
-	},
-	{
-		id: 2,
-		nome: 'Mouse',
-		preco: 120
-	},
-	{
-		id: 3,
-		nome: 'Monitor',
-		preco: 950
-	},
+    {
+        id: 1,
+        nome: 'Tablet Pad SE 128gb 6 de ram Wi Fi',
+        preco: 2199
+    },
+    {
+        id: 2,
+        nome: 'Tablet Positivo Tela 10.1 pol 32gb 2gb',
+        preco: 1199
+    },
+    {
+        id: 3,
+        nome: 'Tablet Multilaser M7 32GB, Android 11, Dual Chip 3G',
+        preco: 527
+    },
+    {
+        id: 4,
+        nome: 'Smartphone Samsung Galaxy A03 Core 32GB',
+        preco: 649
+    },
+    {
+        id: 5,
+        nome: 'Smartphone Samsung Galaxy A14 64 GB',
+        preco: 854
+    },
+    {
+        id: 6,
+        nome: 'Smartphone Motorola Moto E22',
+        preco: 899
+    }
+
+
+
 ]
