d2f9529786a4fa6bcf1aa0058eb6485e74fec12d
NathanNunesCalsonari <rm552539@fiap.com.br>
2023-09-15 19:42:55 -0300

    Criando EditarProduto

---
+++ src/components/EditarProduto.jsx
@@ -0,0 +1,26 @@
+import { useParams, useNavigate } from "react-router-dom"
+import { listaProdutos } from "./ListaProdutos"
+
+export default function EditarProduto(){
+
+    const lista = listaProdutos
+    const navegacao = useNavigate()
+    const {id} = useParams()
+
+    const proc = lista.filter(prod => prod.id == id)
+    const produto = proc[0]
+
+    const salvar = ()=>{
+        
+        return navegacao('/VisualizarAparelhos')
+
+    }
+
+    return(
+        <main>
+            <h1>Deseja ver detalhes do produto a baixo?</h1>
+            <p>{produto.nome}</p>
+            <button onClick={salvar}>Ver Detalhes</button>
+        </main>
+    )
+}
