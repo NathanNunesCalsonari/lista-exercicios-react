0dcc10b3f72a2435d7de682b77f926593fbd484c
NathanNunesCalsonari <rm552539@fiap.com.br>
2023-09-15 21:28:39 -0300

    Atualizando o index do home

--- src/routes/EditarProdutos/index.jsx
+++ src/routes/EditarProdutos/index.jsx
@@ -1,19 +1,15 @@
-import {useParams, useNavigate } from "react-router-dom"
+import {useParams} from "react-router-dom"
 import { ListaProdutos } from "../../components/ListaProdutos"
 
 export default function EditarProduto() {
 
   const lista = ListaProdutos
-  const navegacao = useNavigate()
   const {id} = useParams()
 
   const proc = lista.filter(prod => prod.id == id)
   const produto = proc[0]
   
-  const salvar = () => {
-    alert(`Produto: ${produto.nome} editado com sucesso!`)
-    return navegacao('/produtos')
-  }
+
   
   return (
     <main>
